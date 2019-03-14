8a7187d248331f897674dfacf199f9bd35fb10cc
kr6244 <kaushikraj6244@gmail.com>
2019-03-14 11:12:30 +0530

    first commit kr

---
+++ express-udemy.js
@@ -0,0 +1,71 @@
+//you can render static or dynamic content .for static content like html,css,js that has no variable u can use middleware "express.static(__dirname+'/')"
+//app.use(express.static(dirname+'/'))
+
+const express=require('express');
+const fs=require('fs');
+var hbs=require('express-hbs');
+const _ =require('lodash');
+var app=express();
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine','hbs');
+app.set('views', __dirname + '/views');
+app.get('/about',(req,res)=>
+{
+  res.send('about');
+
+
+});
+
+
+app.get('/',(req,res)=>
+{
+  res.render('try.hbs');
+//  res.send('WELCOME');
+});
+
+
+app.get('/login/:id/:pwd',(req,res)=>
+{
+
+var notes=JSON.parse(fs.readFileSync('login.json'));
+var valid=notes.filter((note)=>
+{
+  if(note.id==req.params.id && note.pwd==req.params.pwd)
+  {
+    res.render('welcome.hbs',{
+      id:req.params.id,
+      currtime: new Date().getFullYear()
+    });
+  }
+  else {
+    res.render('sorry.hbs',{
+      id:req.params.id,
+      currtime: new Date().getFullYear()
+    });
+  }
+});
+
+
+});
+
+
+app.get('/signup/:id/:pwd',(req,res)=>
+{
+  var notes=JSON.parse(fs.readFileSync('login.json'));
+  var newuser={
+    id:req.params.id,
+    pwd:req.params.pwd
+  };
+  notes.push(newuser);
+  fs.writeFileSync('login.json',JSON.stringify(notes));
+  res.send("added user");
+
+})
+
+
+app.listen(4000,()=>
+{
+  console.log('connected to server');
+})
